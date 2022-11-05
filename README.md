2a26e2ac9759d9e25e87462c20655ede7e5cd68a
Dariusz Setlak <kryptoprogramista89@gmail.com>
2022-11-05 03:15:45 +0100

    Next.js project configuration done

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# portfolio-decentralized-lottery-nextjs
-My portfolio project: Decentralized Lottery Dapp - Next.js frontend
+# My portfolio project: Decentralized Lottery Dapp - Next.js frontend
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Decentralized Lottery</title>
+                <meta name="description" content="My SmartContract Decentralized Lottery" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            Hello :-)
+        </div>
+    );
+}
