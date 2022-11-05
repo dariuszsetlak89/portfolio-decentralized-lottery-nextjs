2cdd7a22ac53c544f2516650c5c6ade8c64a2bb8
Dariusz Setlak <kryptoprogramista89@gmail.com>
2022-11-05 03:37:47 +0100

    Header component done

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+const contractAddresses = require("./contractAddresses.json");
+const lotteryAbi = require("./Lottery.json");
+
+module.exports = {
+    contractAddresses,
+    lotteryAbi,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
+import Header from "../components/Header";
+import { useMoralis } from "react-moralis";
+
+const supportedChains = ["31337", "5"];
 
 export default function Home() {
+    const { isWeb3Enabled, chainId } = useMoralis();
+
     return (
         <div>
             <Head>
@@ -8,7 +14,23 @@ export default function Home() {
                 <meta name="description" content="My SmartContract Decentralized Lottery" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            Hello :-)
+            <Header />
+            {isWeb3Enabled ? (
+                <div>
+                    {supportedChains.includes(parseInt(chainId).toString()) ? (
+                        <div className="flex flex-row">
+                            {/* <LotteryEntrance className="p-8" /> */}
+                        </div>
+                    ) : (
+                        <div>
+                            <p>{`Please switch to a supported chain.`}</p>
+                            <p>{`The supported chain ids are: ${supportedChains}`}</p>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <div>Please connect to a Wallet!</div>
+            )}
         </div>
     );
 }
